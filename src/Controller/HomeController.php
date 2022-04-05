cfa64e548517ff68fff498c4d5f764f5b80875a7
Rezzoug Yannis <rezzoug.yannis@gmail.com>
2022-04-05 20:21:08 +0200

    env var

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,11 +28,32 @@ class HomeController extends AbstractController
     */
     public function home()
     {
-        $client_id = $_ENV['SPOTIFY_CLIENT_ID'];
-        $redirect_uri = $_ENV['REDIRECT_URI'];
         $token = $_ENV['SPOTIFY_TOKEN'];
+        $response = $this->client->request(
+            'GET',
+            'https://api.spotify.com/v1/me/playlists',
+            [
+                "auth_bearer" => $token
+            ]
+        );
 
+        $array = json_decode($response->getContent(), true);
 
-        return new Response("Hello World");
+        $result = [];
+
+        foreach($array["items"] as $track) {
+            $result[] = [
+                "id_playlist" => $track["id"],
+                "name" => $track["name"],
+                "spotify_href" => $track["external_urls"]["spotify"],
+                "api_href" => $track["href"],
+                "img_music" => $track["images"],
+                "description" => $track["description"],
+                "total_music" => $track["tracks"]["total"],
+                "uri" => $track["uri"]
+            ];
+        }
+
+        return new JsonResponse($result);
     }
 }
